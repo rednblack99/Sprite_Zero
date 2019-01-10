196512379303a5a98aa37cf06fb9fdc345bcfac3
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-01-10 11:57:26 +0000

    Added sign in view

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -5,10 +5,21 @@ feature 'User Authentication' do
       expect(page).to have_content('Username:')
       expect(page).to have_content('Age:')
       expect(page).to have_content('Password:')
-      fill_in :username, with: 'awesomedave'
-      fill_in :age, with: 16
-      fill_in :password, with: 'Secret123'
+      fill_in :sign_up_username, with: 'awesomedave'
+      fill_in :sign_up_age, with: 16
+      fill_in :sign_up_password, with: 'Secret123'
       click_button 'Sign Up'
+    end
+  end
+
+  context 'Signin' do
+    scenario 'User can signin' do
+      visit '/'
+      expect(page).to have_content('Username:')
+      expect(page).to have_content('Password:')
+      fill_in :sign_in_username, with: 'awesomedave'
+      fill_in :sign_in_password, with: 'Secret123'
+      click_button 'Sign In'
     end
   end
 end
